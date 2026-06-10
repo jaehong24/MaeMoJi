aaaae040d1060bdf1c89fa742b34027aff128fe5
icandogo1 <icandogo1@naver.com>
2026-06-11 00:09:39 +0900

    vntl

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'app.dart';
@@ -5,6 +6,7 @@ import 'services/auth_session_store.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await AuthSessionStore.instance.load();
   runApp(const MaeMojiApp());
 }
